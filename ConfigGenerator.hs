715364f73506f90560cf8d52c07dd977bcd7a4a3
Matthias Krull <m.krull@uninets.eu>
2013-10-25 15:28:10 +0200

    commented code

--- ConfigGenerator.hs
+++ ConfigGenerator.hs
@@ -7,9 +7,12 @@ import System.Console.GetOpt
 import Data.List
 import qualified Geckofinger.StringTemplate as ST
 
+-- flag datatype
+-- derive Eq and Ord to sort command line arguments
 data Flag = TemplateFile String | JSONFile String | AngleTemplate
                                       deriving (Show, Eq, Ord)
 
+-- description of the command line parameters
 options :: [OptDescr Flag]
 options = [
           Option "t" ["template"] (ReqArg TemplateFile "<filename>") "StringTemplate file",
@@ -17,10 +20,11 @@ options = [
           Option "a" ["angle"] (NoArg AngleTemplate) "Template uses angles instead of dollar delimiters"
           ]
 
-parseArgs :: [String] -> String -> IO ([Flag], [String])
+-- parse arguments into a list of flags
+parseArgs :: [String] -> String -> IO [Flag]
 parseArgs argv name =
    case getOpt RequireOrder options argv of
-      (o,n,[]  ) -> return (o,n)
+      (o,_,[]  ) -> return o
       (_,_,errs) -> ioError (userError (concat errs ++ info name))
 
 info :: String -> String
@@ -31,15 +35,20 @@ info n = usageInfo (header n) options
 
 main :: IO ()
 main = do
+  -- get command line arguments, program name
+  -- and parse arguments into a list of flags
   args <- getArgs
   name <- getProgName
   opts <- parseArgs args name
-  case tupleifyOpts $ sort $ fst opts of
+  case tupleifyOpts $ sort opts of
     Just o -> do
+        -- generate string from template file and json file
         config <- ST.mergeSourceFiles o
         case config of
           Just c -> success c
           Nothing -> failure "Failed to generate config"
+    -- if list of flags does fall through tupleifyOpts
+    -- some parameters are missing
     Nothing -> failure $ info name
   where
     success :: String -> IO ()
@@ -51,6 +60,8 @@ main = do
       hPutStrLn stderr s
       exitFailure
 
+-- Nothing if not at least a template file and a json file
+-- are given on the command line
 tupleifyOpts :: [Flag] -> Maybe (String, String, Bool)
 tupleifyOpts (TemplateFile t:JSONFile j:[]) = Just (t, j, False)
 tupleifyOpts (TemplateFile t:JSONFile j:AngleTemplate:[]) = Just (t, j, True)
--- Geckofinger/StringTemplate.hs
+++ Geckofinger/StringTemplate.hs
@@ -9,8 +9,10 @@ import Data.Map as Map (assocs)
 import qualified Data.ByteString.Lazy as LBS (ByteString)
 import Geckofinger.FileIO (safeReadFile, safeReadFileLBS)
 
+-- provide a tuple of two filenames and a bool to indicate the
+-- type of template used
+-- filenames must be in order: template file and json file
 mergeSourceFiles :: (String, String, Bool) -> IO (Maybe String)
--- at least two arguments needed
 mergeSourceFiles (tpl, vars, angle) = do
   template <- safeReadFile tpl
   values <- safeReadFileLBS vars
@@ -19,11 +21,12 @@ mergeSourceFiles (tpl, vars, angle) = do
     Just t -> case values of
         Nothing -> return Nothing
         Just v -> return $ mergeSourceStrings t v angle
--- wrong number of arguments
 mergeSourceFiles _ = return Nothing
 
 -- provide the raw string to create a template and the json values
 -- as lazy bytestring
+-- provide a boolean value to indicate if the template should
+-- use angle brackets instead of the default dollar sign delimiters
 mergeSourceStrings :: String -> LBS.ByteString -> Bool -> Maybe String
 mergeSourceStrings tpl jsonv angle = case decode jsonv of
     Just v -> return $ renderWithValues (assocs v) (newTpl angle tpl)
